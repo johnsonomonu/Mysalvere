846904cdcf19ed5349033b0dcab5c3b6451b7166
romeocodeit <timothydivine9@gmail.com>
2026-05-01 05:51:34 +0100

    chore: update author info and trigger deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 /**
  * Salvere Landing Page
- * Author: romeocodeit
+ * Author: romeocodeit (timothydivine9@gmail.com)
  */
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
